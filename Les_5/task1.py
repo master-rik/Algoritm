ed7941af8391ecec55d0c8e8dcc9e995cd967f23
kant <master-rik@mail.ru>
2020-11-20 16:58:23 +0300

    Homework Les_5

---
+++ Les_5/task1.py
@@ -0,0 +1,34 @@
+"""
+1. Пользователь вводит данные о количестве предприятий,
+их наименования и прибыль за 4 квартал (т.е. 4 числа) для каждого
+предприятия. Программа должна определить среднюю прибыль
+(за год для всех предприятий) и отдельно вывести наименования предприятий,
+чья прибыль выше среднего и ниже среднего.
+"""
+
+from collections import namedtuple
+
+n = int(input('Введите количество предприятий: '))
+Company = namedtuple('Company', 'name, profit')
+result = []
+total = 0
+
+if n != 0:
+    for comp in range(n):
+        name = input(f'Введите название {comp + 1} предприятия :')
+        profit = 0
+        for i in range(4):
+            profit += float(input(f'Введите прибыль предприятия за {i + 1} квартал: '))
+        result.append(Company(name, profit))
+        total += profit
+
+    middle_profit = (total / n)
+    print(f'Средняя прибыль = {middle_profit}')
+
+    for Company in result:
+        if Company[1] > middle_profit:
+            print(f'предприятие {Company[0]} имеет прибыль выше среднего, которая равна {Company[1]}')
+        if Company[1] < middle_profit:
+            print(f'предприятие {Company[0]} имеет прибыль ниже среднего, которая равна {Company[1]}')
+else:
+    print("The end!")
